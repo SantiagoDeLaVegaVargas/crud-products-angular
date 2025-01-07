006aa92821097695cbb5b0232a0503a665038a6e
Blindfox <santiago.delavegavargas@gmail.com>
2025-01-07 09:16:07 -0500

    LocalStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,34 +11,46 @@ import { FormsModule } from '@angular/forms';
 export class AppComponent {
   title = 'crud_products';
 
-  productArray: Product[] = [
-    {id: 1, nombre: "Arroz", precio: 2000, descripcion: "Una libra de arroz blanco"},
-    {id: 2, nombre: "Chocolate", precio: 3000, descripcion: "Una libra de chocolate"}
-  ];
+  productArray: Product[] = [];
 
   newProduct: Product = new Product(0, '', 0, '');
 
+  ngOnInit(): void {
+     this.loadProductsLocalStorage();
+  }
+
   onSubmit() {
-    if (this.newProduct.id === 0) { // id === 0 nuevoo
+    if (this.newProduct.id === 0) { // id === 0 crear
       const newId = this.productArray.length ? this.productArray[this.productArray.length - 1].id + 1 : 1;
       this.newProduct.id = newId;
-      this.productArray.push({...this.newProduct});
-       
+      this.productArray.push({ ...this.newProduct });
+
     } else { // id !== 0 actualizar
       const index = this.productArray.findIndex(product => product.id === this.newProduct.id);
       if (index !== -1) {
-        this.productArray[index] = {...this.newProduct}; 
+        this.productArray[index] = { ...this.newProduct };
       }
     }
-    
+
     this.newProduct = new Product(0, '', 0, '');
+     this.saveProductsLocalStorage();
   }
 
   editProduct(product: Product) {
-    this.newProduct = { ...product }; 
+    this.newProduct = { ...product };
   }
 
   deleteProduct(id: number) {
     this.productArray = this.productArray.filter(product => product.id !== id);
+    this.saveProductsLocalStorage(); 
   }
+
+   saveProductsLocalStorage(): void {
+     localStorage.setItem('productos', JSON.stringify(this.productArray));
+   }
+
+   loadProductsLocalStorage(): void {
+     const savedProducts = localStorage.getItem('productos');
+     this.productArray = savedProducts ? JSON.parse(savedProducts) : [];
+   }
 }
